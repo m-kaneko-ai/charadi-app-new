f9af8b3522891cce88211d07d8d5d294efbdf8dd
m-kaneko-ai <m-kaneko@bringluck.gift>
2025-03-31 16:53:46 +0900

    Fix import statement position and update package dependencies

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './styles.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -15,4 +16,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-import "./styles.css";
+
